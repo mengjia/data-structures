b2bb8c657eb56a5b231252a2d3b3a6a9fc484a5d
unknown <mfgqd@hotmail.com>
2013-06-11 20:11:12 +0800

    init list.java

---
+++ data-structures/src/main/java/org/mengjia/util/List.java
@@ -0,0 +1,36 @@
+package org.mengjia.util;
+
+import java.util.Iterator;
+
+public class List<T> {
+
+	public void add(T value) {
+		
+	}
+	
+	public void remove(T value) {
+		
+	}
+	
+	public void set(int index,T value) {
+		
+	}
+	
+	public T get(int index) {
+		return null;
+	}
+	
+	public int size() {
+		return 0;
+	}
+	
+	public Iterator<T> iterator() {
+		return null;
+	}
+	
+	public T[] toArray() {
+		return null;
+	}
+
+
+}
